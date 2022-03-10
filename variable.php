3d22a827232853a645fff3e7a80d7df0ea0c09d6
LaylaMufahChoiriyah <layla.mfh25@gmail.com>
2022-03-10 09:08:57 +0700

    menambahkan file variable.php

---
+++ variable.php
@@ -0,0 +1,19 @@
+<?php
+    $Angka1 = 8;
+    $Angka2 = 4;
+    //Operasi penjumlahan
+    $penjumlahan = $Angka1 + $Angka2;
+    echo "Penjumlahan ".$Angka1." + ".$Angka2. " = ".$penjumlahan;
+    echo "<br>";
+    //Operasi pengurangan
+    $pengurangan = $Angka1 - $Angka2;
+    echo "Pengurangan ".$Angka1." - ".$Angka2. " = ".$pengurangan;
+    echo "<br>";
+    //Operasi perkalian
+    $perkalian = $Angka1 * $Angka2;
+    echo "Perkalian ".$Angka1." * ".$Angka2. " = ".$perkalian;
+    echo "<br>";
+    //Operasi pembagian
+    $pembagian = $Angka1 / $Angka2;
+    echo "Pembagian ".$Angka1." / ".$Angka2. " = ".$pembagian;
+?>
